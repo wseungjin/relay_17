afb972c5f47355f82c20a927736a0fdcac33622e
Lee SeEun <sally4405@naver.com>
2020-08-07 20:01:54 +0900

    feat: 말풍선 컴포넌트 추가

---
+++ fe/src/components/ChatBot/WordCloud.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WordCloud = (user, text) => {
+  return (
+    <div>
+      <div>말풍선!</div>
+    </div>
+  );
+};
+
+export default WordCloud;
---
+++ fe/src/components/ChatBot/WordCloudList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import WordCloud from "./WordCloud";
+
+const WordCloudList = () => {
+  return (
+    <div>
+      <WordCloud />
+    </div>
+  );
+};
+
+export default WordCloudList;
