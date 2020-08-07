c1b43f96d7694590d8a29c839d65119d1a4913a1
WoWsj <wowtjdwo@gmail.com>
2020-08-07 18:59:07 +0900

    loginpage 1st prototype

--- fe/src/LoginPage.jsx
+++ fe/src/LoginPage.jsx
@@ -1,36 +1,57 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
 export const LoginPage = () => {
-  const [number, setNumber] = useState(0);
-  const [id, setId] = useState("");
-
-  const clickMinus = () => {
-    setNumber(number - 1);
-  };
-
-  const clickPlus = () => {
-    setNumber(number + 1);
-  };
-
-  const changeInput = ({ target }) => {
-    setId(target.value);
-  };
 
   return (
     <div>
-      <div>Login page!!</div>
-
-      <button onClick={clickPlus}>+</button>
-      <MyNumber>{number}</MyNumber>
-      <button onClick={clickMinus}>-</button>
-      <input onChange={changeInput}></input>
-      <div>id: {id}</div>
+    <h1>I Loved School Logo</h1>
+      <LoginBox>
+        <LoginHeader>
+          <h1>로그인</h1>
+          <p>안녕하세요, I Loved School 입니다.</p>
+        </LoginHeader>
+          <form method="post">
+            <div>
+              <Input type="text" name="id" placeholder="아이디"></Input>
+            </div>
+            <div>
+              <Input type="password" name="pw" placeholder="비밀번호"></Input>
+            </div>
+            <div>
+              <Input type="text" name="hobby" placeholder="취미"></Input>
+            </div>
+            <div>
+              <Input type="text" name="graduateYear" placeholder="졸업년도"></Input>
+            </div>
+            <Button type="submit">로그인</Button>
+          </form>
+      </LoginBox>
+      
     </div>
   );
 };
 
-const MyNumber = styled.div`
-  background-color: blue;
-  width: 300px;
-`;
+const Input = styled.input`
+  display : block;
+  margin : 0 auto;
+  margin-top : 10px
+`
+const LoginBox = styled.div`
+  display : block;
+  top : 30%;
+  margin : auto;
+  margin-top : 10%;
+  width : 40%;
+  border : 3px solid grey;
+  padding : 10px;
+`
+const LoginHeader = styled.div`
+  display : block;
+  text-align : center;
+`
+const Button = styled.button`
+display : block;
+margin : 0 auto;
+margin-top : 50px;
+`
