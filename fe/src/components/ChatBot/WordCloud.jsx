65ffa4ac2f5c8c50cc111e77f0299d4968083777
Lee SeEun <sally4405@naver.com>
2020-08-08 16:33:32 +0900

    feat: chatbot user 메세지 왼쪽에 뜨도록 구현

--- fe/src/components/ChatBot/WordCloud.jsx
+++ fe/src/components/ChatBot/WordCloud.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 
-const WordCloud = (user, message) => {
+const WordCloud = ({ message }) => {
   return (
-    <div>
-      <div>{message}</div>
-    </div>
+    <MessageBox isUser={message.user}>
+      <div>{message.text}</div>
+    </MessageBox>
   );
 };
+
+const MessageBox = styled.div`
+  text-align: ${(props) => (props.isUser ? "right" : "left")};
+`;
 
 export default WordCloud;
