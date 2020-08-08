186d107460c3a8d7e6f6ca67cd7699d00a1c280d
Lee SeEun <sally4405@naver.com>
2020-08-08 22:14:32 +0900

    feat: login url 수정 및 유저 정보 조회 api 추가

---
+++ fe/src/constants/url.js
@@ -0,0 +1,6 @@
+export default URL = {
+  login: "http://101.101.219.177:5000/users/insert",
+  userInfoAll: "http://101.101.219.177:5000/users",
+  userInfo: "http://101.101.219.177:5000/users/",
+  chatbot: "",
+};
